e69c0ee8de3b43c6a9430487973ad57d5b543ace
PavelM <reginuser@gmail.com>
2018-06-17 20:20:04 +0300

    1

--- Source/SISPOSProxy.Tests/IntegrationTests/ProxyServiceTests.cs
+++ Source/SISPOSProxy.Tests/IntegrationTests/ProxyServiceTests.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.NetworkInformation;
+﻿using System.Net;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
 using SISPOSProxy.Core.Extentions;
 using SISPOSProxy.Core.Helpers;
-using SISPOSProxy.Core.Models;
 using SISPOSProxy.Tests.Misc;
 
 namespace SISPOSProxy.Tests.IntegrationTests
